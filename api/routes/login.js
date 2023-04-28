203888a2a2382cba985f43f2df06a565fe2fafb6
Nicolas Gonzalez <ng33446@gmail.com>
2023-04-28 14:47:44 -0500

    login user

--- api/routes/login.js
+++ api/routes/login.js
@@ -1,50 +1,29 @@
 import { Router } from 'express';
+import { LoginController } from '../controller/login.controller.js'
 import { body, validationResult } from 'express-validator';
 import { UserModel } from '../models/User.js';
-
+import { loginSchema } from '../schema/validation.schema.js'
+import { validateHandler } from '../middleware/validate.handler.js'
+const loginController = new LoginController();
 export const login = Router();
 
 login.post(
   '/',
   // Validación y sanitización de los datos de entrada
-  body('username').not().isEmpty().trim(),
-  body('password').isLength({ min: 6 }),
-
+  validateHandler(loginSchema, 'body'),
   //
-  async (request, response) => {
+  async (request, response, next) => {
     try {
-      const errors = validationResult(request);
-      if (!errors.isEmpty()) {
-        return response.status(400).json({ errors: errors.array() });
-      }
-
       const { username, password } = request.body;
+      const user = await loginController.loginUser(username, password);
 
-      const user = await UserModel.findOne({ username });
-
-      if (!user) {
-        return response.status(400).json({
-          error: 'username or password is incorrect',
-        });
-      }
-
-      const isPasswordValid = password === user.password;
-      if (!isPasswordValid) {
-        return response.status(400).json({
-          error: 'username or password is incorrect',
-        });
-      }
-
-      // @todo: generate a JWT token
-      const token = 'jwt-token';
-
-      return response.status(201).json({ token, username: user.username });
-    } catch (error) {
-      console.error(`[signIn]: ${error}`);
-
-      return response.status(500).json({
-        error: 'An unexpected error happened. Please try again later',
+      return response.status(201).json({
+        user: user.username,
+        token: user.token
       });
+    } catch (error) {
+      next(error);
+      console.error(`[signIn]: ${error}`);
     }
   }
 );
