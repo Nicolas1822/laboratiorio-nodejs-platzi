540830099abed7be5b38117230c99b1ed02c7980
Nicolas Gonzalez <ng33446@gmail.com>
2023-04-28 14:42:46 -0500

    app file

--- api/app.js
+++ api/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import passport from 'passport';
 import { signUp } from './routes/sign-up.js';
 import { login } from './routes/login.js';
 import { profile } from './routes/profile/index.js';
@@ -12,13 +13,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // API
-// @todo: Almancenar el password de forma segura
+// @todo: Almancenar el password de forma segura (Resuelto)
 app.use('/api/v1/sign-up', signUp);
-// @todo: generar un token jwt seguro para la sesión del usuario
+// @todo: generar un token jwt seguro para la sesión del usuario (Resuelto)
 app.use('/api/v1/login', login);
 // @todo: completar las rutas de profile
-app.use('/api/v1/profile', profile);
+app.use('/api/v1/profile', passport.authenticate('jwt', { session: false }), profile);
 
 app.get('/', async (req, res) => {
   res.send('Platzi laboratio Autenticación con Node.js');
 });
+
