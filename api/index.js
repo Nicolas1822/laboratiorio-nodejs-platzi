ba2cebbd0f70e10fac4b74bc9967f2231721feca
Nicolas Gonzalez <ng33446@gmail.com>
2023-04-28 14:45:29 -0500

    index file

--- api/index.js
+++ api/index.js
@@ -1,10 +1,14 @@
 import 'dotenv/config';
 import { app } from './app.js';
 import { connect as db } from './database.js';
+import { boomErrorLogin } from './middleware/boom.error.js'
 
 db();
 
 const PORT = process.env.PORT || 3000;
+
+import './utils/strategies/strategy.jwt.js';
+app.use(boomErrorLogin);
 
 const server = app.listen(PORT, () =>
   console.log(`[server] Connected to port ${PORT}`)
